f860e491a9c003c1f6d517edbe51c2a952860d51
JepcDev <joseenrique.pc21@gmail.com>
2024-09-12 00:00:09 -0400

    ADD: variables

---
+++ main.py
@@ -0,0 +1,29 @@
+#se puede almacenar string
+#nombre = "Jose"
+#print(nombre)
+
+#nombre = "Juan"
+#print(nombre)
+
+#se puede almacenar numeros
+#edad = 30
+#print(edad)
+
+#edad2 = 10
+#print(edad + edad2)
+
+#ALmacena datos obtenidos de un input
+#nombre = input("Dime tu nombre: ")
+#print("Tu nombre es " + nombre)
+
+#Alamacena datos de dos variables
+#nombre = "Hola"
+#nombre2 = "Python"
+#frase = nombre + " " + nombre2
+#print(frase)
+
+#alamcena resultados de operaciones aritmeticas y demas
+#edad = 20
+#edad2 = 10
+#edadtotal = edad + edad2
+#print(edadtotal)
